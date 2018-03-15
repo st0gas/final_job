9ab7c44d7b0b240afed040d9a7e36c5c3a0337e9
St0gas <4achat@mail.com>
2018-03-15 22:42:20 +0200

    DNv1

--- 16paskaita/js/script.js
+++ 16paskaita/js/script.js
@@ -1,11 +1,13 @@
-function addProd () {
-    var liElement = document.createElement("LI");
-    // var shopText = document.getElementById("textArea").value;
-    var textnode = document.createTextNode(document.getElementById("textArea").value);
-    liElement.appendChild(textnode);
-    document.getElementById("shoppingList").appendChild(liElement);
-    document.getElementById("shoppingList").style.display = "inherit";
-    // console.log (shopText);
-}
+// function addProd () {
+//     var liElement = document.createElement("LI");
+//     // var shopText = document.getElementById("textArea").value;
+//     var textnode = document.createTextNode(document.getElementById("textArea").value);
+//     liElement.appendChild(textnode);
+//     document.getElementById("shoppingList").appendChild(liElement);
+//     document.getElementById("shoppingList").style.display = "inherit";
+//     // console.log (shopText);
+// }
 
+
+var _0xb5a1=["\x4C\x49","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x76\x61\x6C\x75\x65","\x74\x65\x78\x74\x41\x72\x65\x61","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x63\x72\x65\x61\x74\x65\x54\x65\x78\x74\x4E\x6F\x64\x65","\x61\x70\x70\x65\x6E\x64\x43\x68\x69\x6C\x64","\x73\x68\x6F\x70\x70\x69\x6E\x67\x4C\x69\x73\x74","\x64\x69\x73\x70\x6C\x61\x79","\x73\x74\x79\x6C\x65","\x69\x6E\x68\x65\x72\x69\x74"];function addProd(){var _0x9eadx2=document[_0xb5a1[1]](_0xb5a1[0]);var _0x9eadx3=document[_0xb5a1[5]](document[_0xb5a1[4]](_0xb5a1[3])[_0xb5a1[2]]);_0x9eadx2[_0xb5a1[6]](_0x9eadx3);document[_0xb5a1[4]](_0xb5a1[7])[_0xb5a1[6]](_0x9eadx2);document[_0xb5a1[4]](_0xb5a1[7])[_0xb5a1[9]][_0xb5a1[8]]= _0xb5a1[10]}
 
